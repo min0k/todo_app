a8473a530f70ae815232fb0485b17045f4ba8433
min0k <mino.kim@minokim.com>
2022-01-24 16:24:02 -0800

    Refactor MainTodo out of App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from "react";
-import Header from "./ui_components/Header";
-import Todo from "./ui_components/Todos";
-import useTodoList from "./domain_components/useAnimatedGif";
-import useTodoLogic from "./domain_components/useTodoLogic";
-import ClearTasks from "./ui_components/ClearTasks";
+import MainTodo from "./MainTodo";
 
 function App() {
-  const { broom, triggerGif } = useTodoList();
-  const {
-    handleSubmit,
-    handleInput,
-    userInputNewTodo,
-    todos,
-    deleteTask,
-    completeTask,
-    handleClearFinishedTasks,
-  } = useTodoLogic();
-
   return (
     <div className="app">
-      <Header
-        handleSubmit={handleSubmit}
-        handleInput={handleInput}
-        UserInputNewTodo={userInputNewTodo}
-      />
-      <Todo todos={todos} deleteTask={deleteTask} completeTask={completeTask} />
-      <ClearTasks
-        triggerGif={triggerGif}
-        handleClearFinishedTasks={handleClearFinishedTasks}
-        broom={broom}
-      />
+      <MainTodo />
     </div>
   );
 }
