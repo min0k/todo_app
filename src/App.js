2af65f106f5eec6603fae11995a651941223b6d1
min0k <mino.kim@minokim.com>
2022-01-25 21:28:43 -0800

    add task undo functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import MainTodoSection from "./MainTodoSection";
 import PastTodoSection from "./PastTodoSection";
 
+//turn green check into back arrow when task is finished to allow
+//for "unfinishing".
+
+//add todo history clear functionality with an "Are you sure?" popup
+
+//add total completed tasks
+//add show completed date/time on hover
+
 function App() {
   return (
     <div className="app">
--- src/ui_components/TodoCard.js
+++ src/ui_components/TodoCard.js
@@ -1,27 +1,34 @@
 import React from "react";
 
 export default function TodoCard(props) {
-
+  console.log(props);
   const doneStyles = {
     color: "#C8C8C8",
     textDecorationLine: "line-through",
   };
 
   function completeHandle() {
-    props.completeTask(props.id)
+    props.completeTask(props.id);
   }
+
+  const temp = () => {
+    return props.isDone ? "✓" : "hello";
+  }
+
+  const doneEmoji = props.isDone ? "undo" : "✓";
   
+  const undoEmojiStyles = {
+    color: "grey",
+    fontSize: "14px",
+  }
 
   return (
     <div className="todocard">
       <p style={props.isDone ? doneStyles : null} className="todocard--text">
         {props.task}
       </p>
-      <button
-        onClick={completeHandle}
-        className="todocard--button done"
-      >
-        ✓
+      <button style={props.isDone ? undoEmojiStyles : null} onClick={completeHandle} className="todocard--button done">
+       {doneEmoji}
       </button>
     </div>
   );
