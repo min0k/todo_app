f9c2ade11102dc02b123372f796243ad9561b2dc
min0k <mino.kim@minokim.com>
2022-01-27 19:23:56 -0800

    remove nanoid() usage from array element render

--- src/ui_components/Todos.js
+++ src/ui_components/Todos.js
@@ -1,15 +1,15 @@
 import React from "react";
 import TodoCard from "./TodoCard";
-import { nanoid } from "nanoid";
 
 export default function Todo(props) {
+
   const AllTodos = props.todos.map((e) => {
     if (e.isDone === false) {
       return (
         <TodoCard
           isDone={e.isDone}
           task={e.task}
-          key={nanoid()}
+          key={e.id}
           id={e.id}
           completeTask={props.completeTask}
         />
